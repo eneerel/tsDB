9f4facac7c8f9046f8b2769f2dc991da7fc31a68
Evening <evening@mstars-lab3-07s-iMac.local>
2023-04-06 18:27:14 +0800

    pagination

--- controller/movies.ts
+++ controller/movies.ts
@@ -3,10 +3,28 @@ import movie from "../models/ movie";
 
 const getMovies = async (req: Request, res: Response) => {
   try {
-    const movies = await movie.find({}).limit(10);
-    res.status(200).json({ success: true, movies });
+    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page) || 0;
+    const total = await movie.countDocuments();
+    const pageCount = Math.ceil(total / limit);
+    const skip = (page + 1) * limit;
+    console.log("Total", pageCount);
+    console.log("Skip", skip);
+    const movies = await movie.find({}).skip(skip).limit(Number(limit));
+    res
+      .status(200)
+      .json({ success: true, movies, pagination: { total, pageCount, page } });
   } catch (error) {
     console.log("ERROR", error);
   }
 };
-export { getMovies };
+const getMovie = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const movie1 = await movie.findById({ id });
+    res.status(200).json({ success: true, movie1 });
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+};
+export { getMovies, getMovie };
--- routes/moviesRoutes.ts
+++ routes/moviesRoutes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getMovies } from "../controller/movies";
+import { getMovies, getMovie } from "../controller/movies";
 
 const router = Router();
 
 router.route("/").get(getMovies);
-
+router.route("/:id").get(getMovie);
 export default router;
